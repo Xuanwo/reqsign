7212562064955c7d4d851c868c5231ca021268fa
Xuanwo <github@xuanwo.io>
2022-03-16 21:42:26 +0800

    fix: Port in url not added into CanonicalRequest
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/request.rs
+++ src/request.rs
@@ -3,12 +3,23 @@ use http::header::HeaderName;
 use http::{HeaderMap, Method};
 
 pub trait SignableRequest {
+    // Read operations.
     fn method(&self) -> &http::Method;
     fn path(&self) -> &str;
     fn query(&self) -> Option<&str>;
-    fn authority(&self) -> &str;
+    fn host(&self) -> &str;
+    fn port(&self) -> Option<usize>;
     fn headers(&self) -> &http::HeaderMap;
 
+    fn host_port(&self) -> String {
+        if let Some(port) = self.port() {
+            format!("{}:{}", self.host(), port)
+        } else {
+            self.host().to_string()
+        }
+    }
+
+    // Write operations.
     fn apply_header(&mut self, name: HeaderName, value: &str) -> Result<()>;
 }
 
@@ -28,9 +39,14 @@ impl SignableRequest for reqwest::Request {
         this.url().query()
     }
 
-    fn authority(&self) -> &str {
+    fn host(&self) -> &str {
         let this = self as &reqwest::Request;
         this.url().host_str().expect("request uri must have host")
+    }
+
+    fn port(&self) -> Option<usize> {
+        let this = self as &reqwest::Request;
+        this.url().port().map(|v| v as usize)
     }
 
     fn headers(&self) -> &HeaderMap {
@@ -61,12 +77,14 @@ impl<T> SignableRequest for http::Request<T> {
         this.uri().query()
     }
 
-    fn authority(&self) -> &str {
+    fn host(&self) -> &str {
         let this = self as &http::Request<T>;
-        this.uri()
-            .authority()
-            .expect("request uri must have authority")
-            .as_str()
+        this.uri().host().expect("request uri must have host")
+    }
+
+    fn port(&self) -> Option<usize> {
+        let this = self as &http::Request<T>;
+        this.uri().port_u16().map(|v| v as usize)
     }
 
     fn headers(&self) -> &HeaderMap {
