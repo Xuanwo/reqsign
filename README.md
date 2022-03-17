6c8c30305093592b2eb9ea605ac047754c5bf123
Xuanwo <github@xuanwo.io>
2022-03-17 11:54:23 +0800

    docs: Add doc for all public types and mods (#23)
    * docs: Add doc for all public types and mods
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix test
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix doctest
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # reqsign
 
-Signing HTTP requests without heavy SDKs.
+Signing API requests without effort.
+
+---
+
+Most API is simple. But they could be complicated when they are hidden from complex abstraction. `reqsign` bring the simple API back: build, sign, send.
+
+## Quick Start
+
+```rust
+use reqsign::services::aws::v4::Signer;
+use reqwest::{Client, Request, Url};
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    // Signer will load region and credentials from environment by default.
+    let signer = Signer::builder().service("s3").build().await?;
+    // Construct request
+    let url = Url::parse( "https://s3.amazonaws.com/testbucket")?;
+    let mut req = reqwest::Request::new(http::Method::GET, url);
+    // Signing request with Signer
+    signer.sign(&mut req).await?;
+    // Sending already signed request.
+    let resp = Client::new().execute(req).await?;
+    println!("resp got status: {}", resp.status());
+    Ok(())
+}
+```
+
+## Features
+
+- Pure rust with minimal dependencies.
+- Test again official SDK and services.
 
 ## Acknowledge
 
--- src/hash.rs
+++ src/hash.rs
@@ -1,17 +1,25 @@
+//! Hash related utils.
+
 use hmac::Hmac;
 use hmac::Mac;
 use sha2::Digest;
 use sha2::Sha256;
 
+/// SHA256 hash.
 #[allow(dead_code)]
 pub fn sha256(content: &[u8]) -> Vec<u8> {
     Sha256::digest(content).as_slice().to_vec()
 }
 
+/// Hex encoded SHA256 hash.
+///
+/// Use this function instead of `hex::encode(sha256(content))` can reduce
+/// extra copy.
 pub fn hex_sha256(content: &[u8]) -> String {
     hex::encode(Sha256::digest(content).as_slice())
 }
 
+/// HMAC with SHA256 hash.
 pub fn hmac_sha256(key: &[u8], content: &[u8]) -> Vec<u8> {
     let mut h = Hmac::<Sha256>::new_from_slice(key).expect("invalid key length");
     h.update(content);
@@ -19,6 +27,10 @@ pub fn hmac_sha256(key: &[u8], content: &[u8]) -> Vec<u8> {
     h.finalize().into_bytes().to_vec()
 }
 
+/// Hex encoded HMAC with SHA256 hash.
+///
+/// Use this function instead of `hex::encode(hmac_sha256(key, content))` can
+/// reduce extra copy.
 pub fn hex_hmac_sha256(key: &[u8], content: &[u8]) -> String {
     let mut h = Hmac::<Sha256>::new_from_slice(key).expect("invalid key length");
     h.update(content);
