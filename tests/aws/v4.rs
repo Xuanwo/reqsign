aea1c48b90df5e741ce5724ca4db3939d7114b68
Xuanwo <github@xuanwo.io>
2022-05-25 19:37:30 +0800

    feat(aws/v4): Add test for WebIdentityTokenLoader (#58)
    * feat(aws/v4): Add test for WebIdentityTokenLoader
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Help debug
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix CI
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix secret key missing
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Use base64 decode instead
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- tests/aws/v4.rs
+++ tests/aws/v4.rs
@@ -1,9 +1,13 @@
 use anyhow::Result;
 use http::StatusCode;
+use isahc::ReadResponseExt;
 use log::{debug, warn};
+use reqsign::services::aws::loader;
+use reqsign::services::aws::loader::CredentialLoad;
 use reqsign::services::aws::v4::Signer;
-use std::env;
+use serde::Deserialize;
 use std::str::FromStr;
+use std::{env, fs};
 
 fn init_signer() -> Option<Signer> {
     let _ = env_logger::builder().is_test(true).try_init();
@@ -27,6 +31,64 @@ fn init_signer() -> Option<Signer> {
     );
 
     Some(builder.build().expect("signer must be valid"))
+}
+
+#[test]
+fn test_signer_with_web_loader() -> Result<()> {
+    let _ = env_logger::builder().is_test(true).try_init();
+
+    dotenv::from_filename(".env").ok();
+
+    if env::var("REQSIGN_AWS_V4_TEST").is_err() || env::var("REQSIGN_AWS_V4_TEST").unwrap() != "on"
+    {
+        return Ok(());
+    }
+
+    let role_arn = env::var("REQSIGN_AWS_ROLE_ARN").expect("REQSIGN_AWS_ROLE_ARN not exist");
+    let idp_url = env::var("REQSIGN_AWS_IDP_URL").expect("REQSIGN_AWS_IDP_URL not exist");
+    let idp_content =
+        base64::decode(env::var("REQSIGN_AWS_IDP_BODY").expect("REQSIGN_AWS_IDP_BODY not exist"))?;
+
+    let mut req = http::Request::new(idp_content);
+    *req.method_mut() = http::Method::POST;
+    *req.uri_mut() = http::Uri::from_str(&idp_url)?;
+    req.headers_mut()
+        .insert(http::header::CONTENT_TYPE, "application/json".parse()?);
+
+    #[derive(Deserialize)]
+    struct Token {
+        access_token: String,
+    }
+    let token = isahc::HttpClient::new()?
+        .send(req)?
+        .json::<Token>()?
+        .access_token;
+
+    let file_path = format!(
+        "{}/testdata/services/aws/web_identity_token_file",
+        env::current_dir()
+            .expect("current_dir must exist")
+            .to_string_lossy()
+    );
+    fs::write(&file_path, token)?;
+
+    temp_env::with_vars(
+        vec![
+            ("AWS_ROLE_ARN", Some(&role_arn)),
+            ("AWS_WEB_IDENTITY_TOKEN_FILE", Some(&file_path)),
+        ],
+        || {
+            let l = loader::WebIdentityTokenLoader {};
+            let x = l
+                .load_credential()
+                .expect("load_credential must success")
+                .expect("credential must be valid");
+
+            assert!(x.is_valid());
+        },
+    );
+
+    Ok(())
 }
 
 #[tokio::test]
