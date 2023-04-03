ecc16692112055c618fc87886a0c861729cbad58
Xuanwo <github@xuanwo.io>
2023-04-03 18:46:06 +0800

    refactor: Polish API for obs (#276)
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/huaweicloud/obs/credential.rs
+++ src/huaweicloud/obs/credential.rs
@@ -1,28 +1,72 @@
+use super::config::Config;
+use anyhow::Result;
 use std::sync::Arc;
-use std::sync::RwLock;
+use std::sync::Mutex;
 
-use crate::credential::Credential;
+/// Credential for obs.
+#[derive(Clone)]
+#[cfg_attr(test, derive(Debug))]
+pub struct Credential {
+    /// Access key id for obs
+    pub access_key_id: String,
+    /// Secret access key for obs
+    pub secret_access_key: String,
+    /// security_token for obs.
+    pub security_token: Option<String>,
+}
 
 /// CredentialLoader will load credential from different methods.
 #[derive(Default)]
 #[cfg_attr(test, derive(Debug))]
 pub struct CredentialLoader {
-    credential: Arc<RwLock<Option<Credential>>>,
+    config: Config,
+
+    credential: Arc<Mutex<Option<Credential>>>,
 }
 
 impl CredentialLoader {
-    /// Set Credential.
-    pub fn with_credential(self, cred: Credential) -> Self {
-        *self.credential.write().expect("lock poisoned") = Some(cred);
-        self
+    /// Create a new loader via config.
+    pub fn new(config: Config) -> Self {
+        Self {
+            config,
+
+            credential: Arc::default(),
+        }
     }
 
-    /// Load credential.
-    pub fn load(&self) -> Option<Credential> {
+    /// Load credential
+    pub async fn load(&self) -> Result<Option<Credential>> {
         // Return cached credential if it's valid.
-        match self.credential.read().expect("lock poisoned").clone() {
-            Some(cred) if cred.is_valid() => Some(cred),
-            _ => None,
+        if let Some(cred) = self.credential.lock().expect("lock poisoned").clone() {
+            return Ok(Some(cred));
         }
+
+        let cred = self.load_inner().await?;
+
+        let mut lock = self.credential.lock().expect("lock poisoned");
+        *lock = cred.clone();
+
+        Ok(cred)
+    }
+
+    async fn load_inner(&self) -> Result<Option<Credential>> {
+        if let Some(cred) = self.load_via_config()? {
+            return Ok(Some(cred));
+        }
+
+        Ok(None)
+    }
+
+    fn load_via_config(&self) -> Result<Option<Credential>> {
+        if let (Some(ak), Some(sk)) = (&self.config.access_key_id, &self.config.secret_access_key) {
+            let cred = Credential {
+                access_key_id: ak.clone(),
+                secret_access_key: sk.clone(),
+                security_token: self.config.security_token.clone(),
+            };
+            return Ok(Some(cred));
+        }
+
+        Ok(None)
     }
 }
--- src/huaweicloud/obs/mod.rs
+++ src/huaweicloud/obs/mod.rs
@@ -1,7 +1,11 @@
 //! Signers for huaweicloud obs service.
 
 mod signer;
-pub use signer::Builder;
-pub use signer::Signer;
+pub use signer::Signer as HuaweicloudObsSigner;
+
+mod config;
+pub use config::Config as HuaweicloudObsConfig;
 
 mod credential;
+pub use credential::Credential as HuaweicloudObsCredential;
+pub use credential::CredentialLoader as HuaweicloudObsCredentialLoader;
