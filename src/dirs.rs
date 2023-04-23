532fe7c072d5e4dbabb82506fcf00bd34e9e845b
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-04-23 16:33:49 +0530

    Move to the smaller, cargo-team maintained `home` crate

--- src/dirs.rs
+++ src/dirs.rs
@@ -9,6 +9,6 @@ pub fn expand_homedir(path: &str) -> Option<String> {
     if !path.starts_with("~/") && !path.starts_with("~\\") {
         Some(path.to_string())
     } else {
-        dirs::home_dir().map(|home| path.replace('~', &home.to_string_lossy()))
+        home::home_dir().map(|home| path.replace('~', &home.to_string_lossy()))
     }
 }
