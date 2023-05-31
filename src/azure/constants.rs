00b18e34e21bba4a607711aaa450647f5d4a356d
Xuanwo <github@xuanwo.io>
2023-05-31 11:35:13 +0800

    fix(azure): API Version should be specified by clients
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/azure/constants.rs
+++ src/azure/constants.rs
@@ -1,7 +1,3 @@
 // Headers used in azure services.
 pub const X_MS_DATE: &str = "x-ms-date";
-pub const X_MS_VERSION: &str = "x-ms-version";
 pub const CONTENT_MD5: &str = "content-md5";
-
-// Env values used in azure services.
-pub const AZURE_VERSION: &str = "2019-12-12";
