25c10c92715f641cd8a4f8bcbebf57f837f42f66
everpcpc <everpcpc@users.noreply.github.com>
2023-08-03 15:03:15 +0800

    feat(huaweicloud): support loading credential from env (#361)

--- src/huaweicloud/constants.rs
+++ src/huaweicloud/constants.rs
@@ -1,2 +1,7 @@
 // Headers used in huawei cloud services.
 pub const CONTENT_MD5: &str = "Content-MD5"; // different from azure
+
+// Env values used in huawei cloud services.
+pub const HUAWEI_CLOUD_ACCESS_KEY_ID: &str = "HUAWEI_CLOUD_ACCESS_KEY_ID";
+pub const HUAWEI_CLOUD_SECRET_ACCESS_KEY: &str = "HUAWEI_CLOUD_SECRET_ACCESS_KEY";
+pub const HUAWEI_CLOUD_SECURITY_TOKEN: &str = "HUAWEI_CLOUD_SECURITY_TOKEN";
--- src/huaweicloud/obs/config.rs
+++ src/huaweicloud/obs/config.rs
@@ -1,3 +1,8 @@
+use std::collections::HashMap;
+use std::env;
+
+use super::super::constants::*;
+
 /// Config carries all the configuration for Huawei Cloud OBS services.
 #[derive(Clone, Default)]
 #[cfg_attr(test, derive(Debug))]
@@ -5,13 +10,35 @@ pub struct Config {
     /// `access_key_id` will be loaded from
     ///
     /// - this field if it's `is_some`
+    /// - env value: [`HUAWEI_CLOUD_ACCESS_KEY_ID`]
     pub access_key_id: Option<String>,
     /// `secret_access_key` will be loaded from
     ///
     /// - this field if it's `is_some`
+    /// - env value: [`HUAWEI_CLOUD_SECRET_ACCESS_KEY`]
     pub secret_access_key: Option<String>,
     /// `security_token` will be loaded from
     ///
     /// - this field if it's `is_some`
+    /// - env value: [`HUAWEI_CLOUD_SECURITY_TOKEN`]
     pub security_token: Option<String>,
+}
+
+impl Config {
+    /// Load config from env.
+    pub fn from_env(mut self) -> Self {
+        let envs = env::vars().collect::<HashMap<_, _>>();
+
+        if let Some(v) = envs.get(HUAWEI_CLOUD_ACCESS_KEY_ID) {
+            self.access_key_id.get_or_insert(v.clone());
+        }
+        if let Some(v) = envs.get(HUAWEI_CLOUD_SECRET_ACCESS_KEY) {
+            self.secret_access_key.get_or_insert(v.clone());
+        }
+        if let Some(v) = envs.get(HUAWEI_CLOUD_SECURITY_TOKEN) {
+            self.security_token.get_or_insert(v.clone());
+        }
+
+        self
+    }
 }
