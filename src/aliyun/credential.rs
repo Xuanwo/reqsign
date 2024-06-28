3a5d6d512c4b31ba9ab1954f7112bfa5bedb8cca
congyi <15605187270@163.com>
2024-06-28 15:38:00 +0800

    typo

--- src/aliyun/credential.rs
+++ src/aliyun/credential.rs
@@ -166,7 +166,7 @@ impl Loader {
 
     fn get_sts_endpoint(&self) -> String {
         match &self.config.sts_endpoint {
-            Some(definde_sts_endpoint) => format!("https://{}", definde_sts_endpoint),
+            Some(defined_sts_endpoint) => format!("https://{}", defined_sts_endpoint),
             None => "https://sts.aliyuncs.com".to_string(),
         }
     }
