63ce993ddce77f890a10283a4c7f8c767a73775a
Li Yazhou <me.ssword@gmail.com>
2024-08-06 14:25:05 +0800

    fix: add constraints

--- src/sign.rs
+++ src/sign.rs
@@ -1,12 +1,13 @@
+use std::fmt::Debug;
 use std::time::Duration;
 
 /// On some cases like https://github.com/apache/iceberg-rust/issues/506, user may want to
 /// implement their own signing logic for their own services. This trait is defined to
 /// allow user to inject their own signing logic.
 #[async_trait::async_trait]
-pub trait Sign: 'static {
+pub trait Sign: Debug + Send + Sync + Unpin + 'static {
     /// Credential type for this signer.
-    type Credential;
+    type Credential: Debug + Send + Sync + Unpin + 'static;
 
     /// Sign the request with headers.
     async fn sign(
