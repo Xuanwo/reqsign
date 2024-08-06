7535680dd59e1d3ed11b4c55fcf5092a59bbf5b5
Tobias Puetz <tobias@hansetag.com>
2024-08-06 11:37:36 +0200

    make tokio optional and introduce blocking_io feature

--- src/io.rs
+++ src/io.rs
@@ -1,6 +1,9 @@
 pub async fn read_file_to_string(path: &str) -> std::io::Result<String> {
-    #[cfg(target_arch = "wasm32")]
+    #[cfg(any(target_arch = "wasm32", feature = "blocking_io"))]
     return std::fs::read_to_string(path);
-    #[cfg(not(target_arch = "wasm32"))]
+    #[cfg(all(
+        not(target_arch = "wasm32"),
+        all(feature = "tokio_fs", not(feature = "blocking_io"))
+    ))]
     return tokio::fs::read_to_string(path).await;
 }
