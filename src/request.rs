d78f5832a6eacbba55e890455b7f3a554f13f9f8
Li Yazhou <me.ssword@gmail.com>
2024-08-06 12:44:17 +0800

    refactor: use Parts

--- src/request.rs
+++ src/request.rs
@@ -25,78 +25,23 @@ pub trait SignableRequest {
 /// Implement `SignableRequest` for [`http::Request`]
 impl<T> SignableRequest for http::Request<T> {
     fn build(&mut self) -> Result<SigningContext> {
-        let this = self as &mut http::Request<T>;
-
-        let uri = mem::take(this.uri_mut()).into_parts();
-        let paq = uri
-            .path_and_query
-            .unwrap_or_else(|| PathAndQuery::from_static("/"));
-
-        Ok(SigningContext {
-            method: this.method().clone(),
-            scheme: uri.scheme.unwrap_or(Scheme::HTTP),
-            authority: uri
-                .authority
-                .ok_or_else(|| anyhow!("request without authority is invalid for signing"))?,
-            path: paq.path().to_string(),
-            query: paq
-                .query()
-                .map(|v| {
-                    form_urlencoded::parse(v.as_bytes())
-                        .map(|(k, v)| (k.into_owned(), v.into_owned()))
-                        .collect()
-                })
-                .unwrap_or_default(),
-
-            // Take the headers out of the request to avoid copy.
-            // We will return it back when apply the context.
-            headers: mem::take(this.headers_mut()),
-        })
+        let (mut parts, _) = http::request::Request::new(()).into_parts();
+        parts.method = self.method().clone();
+        parts.uri = mem::take(self.uri_mut());
+        parts.version = self.version();
+        parts.headers = mem::take(self.headers_mut());
+        parts.extensions = mem::take(self.extensions_mut());
+        parts.build()
     }
 
-    fn apply(&mut self, mut ctx: SigningContext) -> Result<()> {
-        let this = self as &mut http::Request<T>;
-
-        let query_size = ctx.query_size();
-
-        // Return headers back.
-        mem::swap(this.headers_mut(), &mut ctx.headers);
-
-        let mut parts = mem::take(this.uri_mut()).into_parts();
-        // Return scheme bakc.
-        parts.scheme = Some(ctx.scheme);
-        // Return authority back.
-        parts.authority = Some(ctx.authority);
-        // Build path and query.
-        parts.path_and_query = {
-            let paq = if query_size == 0 {
-                ctx.path
-            } else {
-                let mut s = ctx.path;
-                s.reserve(query_size + 1);
-
-                s.push('?');
-                for (i, (k, v)) in ctx.query.iter().enumerate() {
-                    if i > 0 {
-                        s.push('&');
-                    }
-
-                    s.push_str(k);
-                    if !v.is_empty() {
-                        s.push('=');
-                        s.push_str(v);
-                    }
-                }
-
-                s
-            };
-
-            Some(PathAndQuery::from_str(&paq)?)
-        };
-
-        *this.uri_mut() = Uri::from_parts(parts)?;
-
-        Ok(())
+    fn apply(&mut self, ctx: SigningContext) -> Result<()> {
+        let (mut parts, _) = http::request::Request::new(()).into_parts();
+        parts.method = self.method().clone();
+        parts.uri = mem::take(self.uri_mut());
+        parts.version = self.version();
+        parts.headers = mem::take(self.headers_mut());
+        parts.extensions = mem::take(self.extensions_mut());
+        parts.apply(ctx)
     }
 }
 
