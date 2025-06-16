5c6414796c3336a4d435aa091fa9b807b20ca684
Xuanwo <github@xuanwo.io>
2025-06-16 18:31:24 +0800

    Format code
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- services/huaweicloud-obs/src/load/config.rs
+++ services/huaweicloud-obs/src/load/config.rs
@@ -22,13 +22,8 @@ impl Load for ConfigLoader {
     type Key = Credential;
 
     async fn load(&self, _: &Context) -> Result<Option<Self::Key>> {
-        if let (Some(ak), Some(sk)) = (&self.config.access_key_id, &self.config.secret_access_key)
-        {
-            let cred = Credential::new(
-                ak.clone(),
-                sk.clone(),
-                self.config.security_token.clone(),
-            );
+        if let (Some(ak), Some(sk)) = (&self.config.access_key_id, &self.config.secret_access_key) {
+            let cred = Credential::new(ak.clone(), sk.clone(), self.config.security_token.clone());
             return Ok(Some(cred));
         }
 
@@ -57,7 +52,7 @@ mod tests {
                     let config = Config::default().from_env();
                     let loader = ConfigLoader::new(config);
                     let ctx = Context::new(TokioFileRead, ReqwestHttpSend::default());
-                    
+
                     let x = loader.load(&ctx).await.expect("load must succeed");
                     let x = x.expect("must load succeed");
                     assert_eq!("access_key_id", x.access_key_id);
