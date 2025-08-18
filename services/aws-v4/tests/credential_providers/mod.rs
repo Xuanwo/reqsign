f0883c3c158565fe308a44a925d1c606f1f6f77d
Xuanwo <github@xuanwo.io>
2025-08-19 02:28:31 +0800

    Format code
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- services/aws-v4/tests/credential_providers/mod.rs
+++ services/aws-v4/tests/credential_providers/mod.rs
@@ -10,9 +10,9 @@ mod profile;
 #[cfg(not(target_arch = "wasm32"))]
 mod sso;
 
-use reqsign_core::{Context, OsEnv, StaticEnv};
 #[cfg(not(target_arch = "wasm32"))]
 use reqsign_command_execute_tokio::TokioCommandExecute;
+use reqsign_core::{Context, OsEnv, StaticEnv};
 use reqsign_file_read_tokio::TokioFileRead;
 use reqsign_http_send_reqwest::ReqwestHttpSend;
 use std::collections::HashMap;
@@ -20,17 +20,17 @@ use std::collections::HashMap;
 pub fn create_test_context() -> Context {
     let _ = env_logger::builder().is_test(true).try_init();
     let _ = dotenv::dotenv();
-    
+
     let mut ctx = Context::new()
         .with_file_read(TokioFileRead)
         .with_http_send(ReqwestHttpSend::default())
         .with_env(OsEnv);
-    
+
     #[cfg(not(target_arch = "wasm32"))]
     {
         ctx = ctx.with_command_execute(TokioCommandExecute);
     }
-    
+
     ctx
 }
 
@@ -45,12 +45,12 @@ pub fn create_test_context_with_env(envs: HashMap<String, String>) -> Context {
         .with_file_read(TokioFileRead)
         .with_http_send(ReqwestHttpSend::default())
         .with_env(OsEnv);
-    
+
     #[cfg(not(target_arch = "wasm32"))]
     {
         ctx = ctx.with_command_execute(TokioCommandExecute);
     }
-    
+
     // StaticEnv overrides specific environment variables on top of OsEnv
     ctx.with_env(StaticEnv { home_dir, envs })
 }
