9eb942cb002005da2c6cd8668eb6a8c700b904d0
Xuanwo <github@xuanwo.io>
2025-08-18 18:14:05 +0800

    Setup HOME
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- services/aws-v4/tests/credential_providers/mod.rs
+++ services/aws-v4/tests/credential_providers/mod.rs
@@ -24,8 +24,12 @@ pub fn create_test_context() -> Context {
 pub fn create_test_context_with_env(envs: HashMap<String, String>) -> Context {
     let _ = env_logger::builder().is_test(true).try_init();
     let _ = dotenv::dotenv();
+    
+    // Get home directory from HOME environment variable if set
+    let home_dir = std::env::var("HOME").ok().map(std::path::PathBuf::from);
+    
     Context::new(TokioFileRead, ReqwestHttpSend::default()).with_env(StaticEnv {
-        home_dir: None,
+        home_dir,
         envs,
     })
 }
